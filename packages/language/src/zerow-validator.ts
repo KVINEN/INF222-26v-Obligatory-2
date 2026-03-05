7540198c626696ea0c616bb012cca3a52c128218
Benjamin Kvinen Røiland <benjamin.roiland@hotmail.no>
2026-03-05 21:13:11 +0100

    added unit & declare + validation for declare

--- packages/language/src/zerow-validator.ts
+++ packages/language/src/zerow-validator.ts
@@ -1,5 +1,5 @@
 import type { ValidationAcceptor, ValidationChecks } from 'langium';
-import type { Program, ZerowAstType } from './generated/ast.js';
+import type { Declare, Program, ZerowAstType } from './generated/ast.js';
 import type { ZerowServices } from './zerow-module.js';
 
 /**
@@ -9,6 +9,7 @@ export function registerValidationChecks(services: ZerowServices) {
     const registry = services.validation.ValidationRegistry;
     const validator = services.validation.ZerowValidator;
     const checks: ValidationChecks<ZerowAstType> = {
+        Declare: validator.checkUniqueDeclaration,
         // TODO: Declare validators for your properties
         Program: validator.checkProgram
     };
@@ -23,6 +24,20 @@ export class ZerowValidator {
     // TODO: Add logic here for validation checks of properties
     checkProgram(model: Program, accept: ValidationAcceptor): void {
         // this.validateProgram(model, accept);
+    }
+
+    checkUniqueDeclaration(declare: Declare, accept: ValidationAcceptor): void {
+        const container = declare.$container as Program;
+
+        const duplicate = container.declare.find(d => d !== declare && d.name === declare.name);
+
+        if (duplicate) {
+            accept('error', `Declaration with variable ' ${declare.name} 'already exist.`, {
+                node: declare,
+                property: 'name'
+            });
+        }
+
     }
 
 
