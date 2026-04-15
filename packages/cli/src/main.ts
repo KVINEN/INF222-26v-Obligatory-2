3856aadc5087c4a48d97a21410d68afc78903c64
Benjamin Kvinen Røiland <benjamin.roiland@hotmail.no>
2026-04-15 10:22:58 +0200

    completed main.ts

--- packages/cli/src/main.ts
+++ packages/cli/src/main.ts
@@ -3,12 +3,13 @@ import { createZerowServices, ZerowLanguageMetaData } from 'zerow-language';
 import chalk from 'chalk';
 import { Command } from 'commander';
 import { extractAstNode } from './util.js';
-import { generateOutput } from './generator.js';
+import { compile, generateOutput } from './generator.js';
 import { NodeFileSystem } from 'langium/node';
 import * as url from 'node:url';
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
 
 const packagePath = path.resolve(__dirname, '..', 'package.json');
 const packageContent = await fs.readFile(packagePath, 'utf-8');
@@ -41,4 +42,17 @@ export default function (): void {
         .action(compileAndRun);
 
     program.parse(process.argv);
+}
+
+function loadMod(bytes: BufferSource) {
+    const mod = new WebAssembly.Module(bytes);
+    return new WebAssembly.Instance(mod).exports;
+}
+
+const compileAndRun = async (source: string): Promise<void> => {
+    const services = createZerowServices(NodeFileSystem).Zerow;
+    const model = await extractAstNode<Program>(source, services);
+    const wasmByteCode = compile(model);
+    let wasmMain = loadMod(wasmByteCode).main as CallableFunction;
+    console.log(chalk.green(`Wasm output: ${wasmMain()}`));
 }
